3789f12ce4d3f14db34b7d6ee676d7a8ede4a973
Mark Goddard <mark@stackhpc.com>
2023-01-09 12:17:23 +0000

    Add signal handler
    Otherwise it hangs on Ctrl-C!

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use axum::{
     routing::get,
     Router,
 };
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
@@ -11,6 +12,33 @@ async fn main() {
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
