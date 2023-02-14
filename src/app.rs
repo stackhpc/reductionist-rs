ffe0da0c8842f33363e0e95b13ef842ace182660
Mark Goddard <mark@stackhpc.com>
2023-02-14 16:20:56 +0000

    Validate presence of authorization request header

--- src/app.rs
+++ src/app.rs
@@ -2,11 +2,18 @@ use crate::models;
 use crate::validated_json::ValidatedJson;
 
 use axum::{
+    body::Body,
     http::header,
+    http::Request,
+    http::StatusCode,
     response::{IntoResponse, Response},
     routing::{get, post},
     Router,
 };
+
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tower_http::validate_request::ValidateRequestHeaderLayer;
 
 static HEADER_DTYPE: header::HeaderName = header::HeaderName::from_static("x-activestorage-dtype");
 static HEADER_SHAPE: header::HeaderName = header::HeaderName::from_static("x-activestorage-shape");
@@ -37,6 +44,20 @@ pub fn router() -> Router {
             .route("/min", post(min))
             .route("/select", post(select))
             .route("/sum", post(sum))
+            .layer(
+                ServiceBuilder::new()
+                    .layer(TraceLayer::new_for_http())
+                    .layer(ValidateRequestHeaderLayer::custom(
+                        // Validate that an authorization header has been provided.
+                        |request: &mut Request<Body>| {
+                            if request.headers().contains_key(header::AUTHORIZATION) {
+                                Ok(())
+                            } else {
+                                Err(StatusCode::UNAUTHORIZED.into_response())
+                            }
+                        },
+                    )),
+            )
     }
 
     Router::new()
