525a77f6e530551eb9ad96e8c19e1bc1483451bb
Mark Goddard <mark@stackhpc.com>
2023-05-02 15:31:24 +0100

    Refactor CLI arguments into separate module

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod app;
 mod array;
+mod cli;
 mod error;
 mod models;
 mod operation;
@@ -40,41 +41,10 @@ mod operations;
 mod s3_client;
 mod validated_json;
 
-/// S3 Active Storage Proxy command line interface
-#[derive(Debug, Parser)]
-struct CommandLineArgs {
-    /// The IP address on which the proxy should listen
-    #[arg(long, default_value = "0.0.0.0", env = "S3_ACTIVE_STORAGE_HOST")]
-    host: String,
-    /// The port to which the proxy should bind
-    #[arg(long, default_value_t = 8080, env = "S3_ACTIVE_STORAGE_PORT")]
-    port: u16,
-    /// Flag indicating whether HTTPS should be used
-    #[arg(long, default_value_t = false, env = "S3_ACTIVE_STORAGE_HTTPS")]
-    https: bool,
-    /// Path to the certificate file to be used for HTTPS encryption
-    #[arg(
-        long,
-        default_value = "~/.config/s3-active-storage/certs/cert.pem",
-        env = "S3_ACTIVE_STORAGE_CERT_FILE"
-    )]
-    cert_file: String,
-    /// Path to the key file to be used for HTTPS encryption
-    #[arg(
-        long,
-        default_value = "~/.config/s3-active-storage/certs/key.pem",
-        env = "S3_ACTIVE_STORAGE_KEY_FILE"
-    )]
-    key_file: String,
-    /// Maximum time in seconds to wait for operations to complete upon receiving `ctrl+c` signal.
-    #[arg(long, default_value_t = 60, env = "S3_ACTIVE_STORAGE_SHUTDOWN_TIMEOUT")]
-    graceful_shutdown_timeout: u64,
-}
-
 /// Application entry point
 #[tokio::main]
 async fn main() {
-    let args = CommandLineArgs::parse();
+    let args = cli::CommandLineArgs::parse();
 
     init_tracing();
 
